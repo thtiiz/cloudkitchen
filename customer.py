63d35742eb8d0b1c31f4d063b6daafe99a0330f3
fordtheerathat <fordnakrong1@gmail.com>
2020-03-20 14:00:58 +0700

    fix files

--- customer.py
+++ customer.py
@@ -4,10 +4,20 @@ from machine import Pin
 import time
 import network
 from esp import espnow
+import json
 # from adafruit_debouncer import Debouncer
+led = Pin(16, Pin.OUT)
 BUTTON_A_PIN = const(17)
 BUTTON_B_PIN = const(5)
 is_served = True
+
+w = network.WLAN()
+w.active(True)
+
+# BROADCAST = b'\x30'+b'\xAE'+b'\xA4'+b'\x12'+b'\x64'+b'\xE0' #AEA41264E0
+BROADCAST = b'\xFF'*6
+espnow.init()
+espnow.add_peer(BROADCAST)
 
 
 class Button:
@@ -39,47 +49,34 @@ class Button:
         #    print("debounce: %s" % (self._next_call - time.ticks_ms()))
 
 
-def send_order():
+def ledIsOn():
     is_served = False
     if(~is_served):
         led.value(1)
     elif(is_served):
         led.value(0)
-    global order = {}
 
 
 def button_a_callback(pin):
-    order[table_num] = 1
-    order[chef_num] = 1
-    print("order1")
-    send_order()
+    print('order1')
+    order = {}
+    order['table_num'] = 1
+    order['chef_num'] = 1
+    msg = json.dumps(order)
+    espnow.send(BROADCAST, msg)
+    ledIsOn()
 
 
 def button_b_callback(pin):
-    order.append(2)
-    print("order2")
-    send_order()
-
-
-w = network.WLAN()
-w.active(True)
-
-BROADCAST = b'\x30AEA412640'
-
-espnow.init()
-espnow.add_peer(BROADCAST)
-
-
-count = 0
-while True:
-    count += 1
-    msg = "Count = {}".format(count)
-    print("Sending:", msg)
+    print('order2')
+    order = {}
+    order['table_num'] = 1
+    order['chef_num'] = 2
+    msg = json.dumps(order)
     espnow.send(BROADCAST, msg)
-    time.sleep(1)
+    ledIsOn()
 
-# button_a = Button(pin=Pin(BUTTON_A_PIN, mode=Pin.IN, pull=Pin.PULL_UP), callback=button_a_callback)
-# button_b = Button(pin=Pin(BUTTON_B_PIN, mode=Pin.IN, pull=Pin.PULL_UP), callback=button_b_callback)
+
 button_a = Button(pin=Pin(BUTTON_A_PIN, mode=Pin.IN,
                           pull=Pin.PULL_UP), callback=button_a_callback)
 button_b = Button(pin=Pin(BUTTON_B_PIN, mode=Pin.IN,
