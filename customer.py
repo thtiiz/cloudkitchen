3bea9deaf0707cce6e5b1cc0181d6686e658f18c
fordtheerathat <fordnakrong1@gmail.com>
2020-03-20 17:11:55 +0700

    deploy 1.0

--- customer.py
+++ customer.py
@@ -21,7 +21,7 @@ ORDER[1] = 0
 ORDER[2] = 0
 PRICE_1 = 40
 PRICE_2 = 50
-# STATE = ''
+STATE = 'ORDER'
 # init WIFI
 w = network.WLAN()
 w.active(True)
@@ -65,14 +65,14 @@ class Button:
 
 def init_display():
     customer_oled.fill(0)
-    msg_table = f'Table {TABLE_NUM}'
+    msg_table = 'Table {}'.format(TABLE_NUM)
     customer_oled.text(msg_table, 0, 0)
     customer_oled.show()
 
 
 def update_cooking():
     customer_oled.fill(0)
-    msg_table = f'Table {TABLE_NUM}'
+    msg_table = 'Table {}'.format(TABLE_NUM)
     customer_oled.text(msg_table, 0, 0)
     customer_oled.show()
     msg = 'Cooking . . .'
@@ -84,57 +84,68 @@ def toggleLED():
     print(is_served)
     if(is_served == False):
         led.value(1)
+        update_cooking()
     elif(is_served):
         led.value(0)
-    update_cooking()
+        init_display()
 
 
 def button_a_callback(pin):
-    order = {}
-    order['table_num'] = 1
-    order['chef_num'] = 1
-    print("order1")
-    msg = json.dumps(order)
-    print("Sending:", msg)
-    espnow.send(BROADCAST, msg)
-    global is_served
-    global COUNT
-    global PRICE
-    global ORDER
-    ORDER[1] += 1
-    COUNT += 1
-    is_served = False
-    toggleLED()
+    global STATE
+    if(STATE == 'ORDER'):
+        order = {}
+        order['table_num'] = 1
+        order['chef_num'] = 1
+        print("order1")
+        msg = json.dumps(order)
+        print("Sending:", msg)
+        espnow.send(BROADCAST, msg)
+        global is_served
+        global COUNT
+        global PRICE
+        global ORDER
+        ORDER[1] += 1
+        COUNT += 1
+        is_served = False
+        toggleLED()
 
 
 def button_b_callback(pin):
-    order = {}
-    order['table_num'] = 1
-    order['chef_num'] = 2
-    print("order2")
-    msg = json.dumps(order)
-    print("Sending:", msg)
-    espnow.send(BROADCAST, msg)
-    global is_served
-    global COUNT
-    global PRICE
-    global ORDER
-    ORDER[2] += 1
-    COUNT += 1
-    is_served = False
-    toggleLED()
+    global STATE
+    if(STATE == 'ORDER'):
+        order = {}
+        order['table_num'] = 1
+        order['chef_num'] = 2
+        print("order2")
+        msg = json.dumps(order)
+        print("Sending:", msg)
+        espnow.send(BROADCAST, msg)
+        global is_served
+        global COUNT
+        global PRICE
+        global ORDER
+        ORDER[2] += 1
+        COUNT += 1
+        is_served = False
+        toggleLED()
 
 
 def button_c_callback(pin):
-    if(COUNT == 0):
-        global PRICE
-        global ORDER
-        PRICE += ORDER[1] * PRICE_1
-        PRICE += ORDER[2] * PRICE_2
-        update_oled()
-        PRICE = 0
-        ORDER[1] = 0
-        ORDER[2] = 0
+    global STATE
+    if(STATE == 'ORDER'):
+        if(COUNT == 0):
+            global PRICE
+            global ORDER
+            PRICE += ORDER[1] * PRICE_1
+            PRICE += ORDER[2] * PRICE_2
+            update_oled()
+            PRICE = 0
+            ORDER[1] = 0
+            ORDER[2] = 0
+            STATE = 'SERVE'
+    elif(STATE == 'SERVE'):
+        init_display()
+        STATE = 'ORDER'
 
 
 def update_oled():
